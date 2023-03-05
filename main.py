7897d9c6b1dca2116926f21ccedeaae9f53270b4
LeeC77 <lee.charlton00@gmail.com>
2023-03-05 18:30:40 +0000

    Added CL options parser

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import sys
 import json
 import time
 import requests  # pip install requests
-import socket
-# import this
-# BT addr for SMA inverter 00:80:25:2F:E8:19
+import argparse
+
 
 ##### import my packages
 #import package1.progress
 import package1.smabluetooth
 
-
+VERSION_STRING = "SMA Bluetooth to openHAB rest API.\n\rVersion: 1.1 Date: 05 Mar 2023"
 DEFAULT_CONFIG_FILE = os.path.expanduser("~/sma.json") # Windows
 #DEFAULT_CONFIG_FILE = os.path.expanduser("./sma.json") # Debian
 # Running on Debian
@@ -70,7 +69,10 @@ def main():
 
     #Get configuration from file
     # load the .json file
-    configfile=None
+    if args.file:
+        configfile = args.file
+        if verbose: print("overriding default config file location")
+    else:configfile=None
 
     if configfile is None:
                 configfile = DEFAULT_CONFIG_FILE
@@ -86,7 +88,6 @@ def main():
     if "system name" in alljson:
         sysjson = alljson["system name"]
         system_name = sysjson.get("name")
-        print("System name: " + system_name)
     if "inverter" in alljson:
         invjson = alljson["inverter"]
         inverter_name = invjson.get("name")
@@ -97,43 +98,91 @@ def main():
         ohjson = alljson["openhab"]
         openhab_key = ohjson.get("apikey")
         openhab_IPport=ohjson.get("IPport")
-        print ("Inverter:")
-        print ("\t\tName: " + inverter_name)
-        print ("\t\tSerial: " + inverter_serial)
-        print ("\t\tBluetooth address: " + inverter_bluetooth)
+        if verbose:
+            print ("System name: " + system_name)
+            print ("Inverter:")
+            print ("\t\tName: " + inverter_name)
+            print ("\t\tSerial: " + inverter_serial)
+            print ("\t\tBluetooth address: " + inverter_bluetooth)
+            print ("\t\tOpenHab URL: " + openhab_IPport)
+            print ("\t\tKeys and Passwords not shown.")
         
         
-    print("Trying BT address: " + inverter_bluetooth )
+    if verbose: print("Trying BT address: " + inverter_bluetooth )
 
-    #Open connectin to inverter get data and send to OpenHAB
+# Open connectin to inverter get data and send to OpenHAB
 
     try:
         sma = connect_and_logon(inverter_bluetooth, password= bytes(inverter_password, 'utf-8'), timeout=900)
-        dtime, daily = sma.daily_yield()
-        send_to_openHAB(daily,openhab_IPport, openhab_key,"total today") # Send Data to openHAB REST interface
-        print("\t\tAt %s Daily generation was:\t %d Wh" % (package1.datetimeutil.format_time(dtime), daily))
-        ttime, total = sma.total_yield()
-        send_to_openHAB(total,openhab_IPport, openhab_key, "total energy") # Send Data to openHAB REST interface
-        print("\t\tAt %s Total generation was:\t %d Wh" % (package1.datetimeutil.format_time(ttime), total))
-    #### LC ##
-        wtime, watts = sma.spot_power() 
-        print("\t\tAt %s Spot Power is:\t\t %d W" % (package1.datetimeutil.format_time(wtime), watts))
-        send_to_openHAB(watts,openhab_IPport, openhab_key, "spot power") # Send Data to openHAB REST interface
-        tetime, temp = sma.spot_temp()
-        print("\t\tAt %s Spot Temperature is:\t %.2f °C" % (package1.datetimeutil.format_time(tetime), temp/100))
-        send_to_openHAB(temp,openhab_IPport, openhab_key, "spot temperature") # Send Data to openHAB REST interface    
         
+        dtime, daily = sma.daily_yield()
+        ttime, total = sma.total_yield()
+        wtime, watts = sma.spot_power() 
+        tetime, temp = sma.spot_temp()
+        
+# Report values verbose
+        if verbose: 
+            print("\t\tAt %s Daily generation was:\t %d Wh" % (package1.datetimeutil.format_time(dtime), daily))
+            print("\t\tAt %s Total generation was:\t %d Wh" % (package1.datetimeutil.format_time(ttime), total))
+            print("\t\tAt %s Spot Power is:\t\t %d W" % (package1.datetimeutil.format_time(wtime), watts))
+            print("\t\tAt %s Spot Temperature is:\t %.2f °C" % (package1.datetimeutil.format_time(tetime), temp/100))
+          
+# Send values to openHAB REST interface        
+        if not(args.openhab_off):
+            if verbose: print("exporting to openHAB API")
+            send_to_openHAB(daily,openhab_IPport, openhab_key,"total today")
+            send_to_openHAB(total,openhab_IPport, openhab_key, "total energy")
+            send_to_openHAB(watts,openhab_IPport, openhab_key, "spot power")
+            send_to_openHAB(temp,openhab_IPport, openhab_key, "spot temperature")
+        else:
+            if verbose: print("Skipping openHAB API export")
         
     except Exception as e:
-        print ("Error  contacting  inverter: %s" % e, file =  sys.stderr)
+        print ("Error contacting inverter: %s" % e, file =  sys.stderr)
         
-    print ("Done: " + inverter_bluetooth)
+    if ((not(verbose)) & (not(args.silent))): print ("Done: \t%s, %dWh, %dMWh, %dW, %.2f°C," %(package1.datetimeutil.format_time(dtime),daily,total/1000000,watts,temp/100))
     
 starttime = time.time()
-while True:
-    print("tick")
-    main()
-    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+go_interval = 30
+
+if __name__ == "__main__":
+        
+#Command line options        
+    parser = argparse.ArgumentParser(description="SMA Inverter Bluetooth  to openHAB rest API",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-c", "--continuous",                           help="run continously checking every x second. x > 10. e.g. py openhabSMA - c 90", type=int, )
+    parser.add_argument("-v", "--verbose",      action="store_true",    help="increase verbosity")
+    parser.add_argument("-f", "--file",                                 help="path to and .jsn configuration filename. e.g py openhabSMA -f ./test/sma.json", type=str)
+    parser.add_argument("-o", "--openhab_off",  action="store_true",    help="turns off rest sends to openHAB")
+    parser.add_argument("-s", "--silent",       action="store_true",    help="completely silent running. -s overrides -v ")
+    parser.add_argument("--version",            action="store_true",    help="report version only, main() doesn't run")
+    parser.add_argument("-l", "--logfile",                              help="TO DO: log to file, for now use pipe. Windows example: py main.py -o > temp.txt")
+    args = parser.parse_args()
+    config = vars(args)
+    if args.version : 
+        print (VERSION_STRING)
+        exit()
+    verbose = args.verbose
+# Override verbose if in silent
+    if args.silent == True: verbose = False
+# Report running mode
+    if verbose: print(config)    
+    else: 
+        if not(args.silent):print("Runnig quietly")
+# Run once or continuously
+    if args.continuous :
+        if args.continuous >= 10 :
+            go_interval = args.continuous
+            if verbose: print("Running once every %d seconds" % (go_interval))
+            while True:
+                main()
+                time.sleep(go_interval - ((time.time() - starttime) % go_interval))
+        else :
+            print ("value provided for CL option -continuous incorrect")
+    else:
+            main()
+
+        
 
 
 
