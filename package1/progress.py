6d775becfc1b5017dcb28e26011265eb81cdc744
LeeC77 <lee.charlton00@gmail.com>
2023-03-18 22:34:16 +0000

    Deletes

--- package1/progress.py
+++
@@ -1,7 +0,0 @@
-def progress_function(BTAddress, result):
-    if (result ==  1):
-        print("Trying BT address: " + BTAddress )
-    if (result == 2):
-        print ("Done: " + BTAddress )
-    #    print (dir(BTAddress))  # prints all the methods of the object
-    return 0
